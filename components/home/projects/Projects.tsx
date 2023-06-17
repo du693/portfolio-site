478067280424e01c19aa3b5864029b308676fbaf
du693 <100497517+du693@users.noreply.github.com>
2023-06-17 12:49:54 -0400

    adding projects

--- components/home/projects/Projects.tsx
+++ components/home/projects/Projects.tsx
@@ -23,7 +23,8 @@ const projects = [
 		code: "https://github.com/du693/job-scraper",
 		projectLink: "",
 		tech: ["Python, Flask, HTML, CSS"],
-		description: "",
+		description:
+			"Currently, the job scraper is local python web scraper that compiles results from popular job boards and displays them in a Flask application. I made this project because navigating through each individual job board was taking up loads of time that I could be using to apply to positions and studying. I figured that this project would not only make the job searching process more efficient but also further develop my skills with Python and app development. While the app is still a work in progress I have already started using and loving it. ",
 		modalContent: (
 			<>
 				<p></p>
@@ -31,12 +32,13 @@ const projects = [
 		),
 	},
 	{
-		title: "Project-Name",
+		title: "Bellabeat Case Study",
 		imgSrc: "project-imgs/bellabeat.jpg",
 		code: "https://github.com/du693/bellabeat-case-study",
 		projectLink: "https://du693.github.io/bellabeat-case-study/",
 		tech: ["R, Rmarkdown, HTML"],
-		description: "",
+		description:
+			"Using the dataset 'FitBit Fitness Tracker Data' provided by MÖBIUS on Kaggle I created a mock case-study where I searched for user patterns of usage in their smart device data in order to gain insights that would later better orientate marketing decisions for Bellabeat, a tech company with a focus in women's smart products.",
 		modalContent: (
 			<>
 				<p></p>
@@ -44,25 +46,15 @@ const projects = [
 		),
 	},
 	{
-		title: "Project-Name",
-		imgSrc: "",
-		code: "https://www.github.com",
-		projectLink: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
-		tech: ["Tech-Here"],
-		description: "",
-		modalContent: (
-			<>
-				<p></p>
-			</>
-		),
-	},
-	{
-		title: "Project-Name",
-		imgSrc: "",
-		code: "https://www.github.com",
-		projectLink: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
-		tech: ["Tech-Here"],
-		description: "",
+		title: "Portfolio Site",
+		imgSrc: "project-imgs/portfolio.jpg",
+		code: "https://github.com/du693/portfolio-site",
+		projectLink: "https://jackcarleo.vercel.app/",
+		tech: [
+			"React, HTML, Typescript, Javascript, Sass, CSS/JS animations, Framer Motion, Next.js, Vercel",
+		],
+		description:
+			"Inspired by the website created by Tom is loading on youtube, this portfolio website was a my first deployed React project incorporating Typescript, Javascript, Scss, Next.js, and vercel.",
 		modalContent: (
 			<>
 				<p></p>
