89c50b93f41cc5b27e4dab743ce4766d010feaa6
du693 <100497517+du693@users.noreply.github.com>
2024-01-03 12:34:45 -0500

    removing speed insights

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,6 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Poppins } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const poppins = Poppins({ weight: ["100", "200", "400", "700", "900"] });
 
@@ -11,7 +10,6 @@ export default function App({ Component, pageProps }: AppProps) {
 		<div id="root" className={poppins.className}>
 			<Component {...pageProps} />
 			<Analytics />
-			<SpeedInsights />
 		</div>
 	);
 }
