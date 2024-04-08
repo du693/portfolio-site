aff51dae1aac6ceb878fa245bf7706ab76af29e4
du693 <100497517+du693@users.noreply.github.com>
2024-04-08 19:13:58 -0400

    fixing mobile ZI update

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "db42279a1d254f07"; }
+/******/ 		__webpack_require__.h = function() { return "45b04e39f1f50e3e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
